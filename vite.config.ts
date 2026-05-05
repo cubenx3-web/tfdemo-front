db2af7b56dd9520e0e1ced7151ac67257848e68d
cubenx3-web <cubenx3@gmail.com>
2026-05-06 02:24:23 +0300

    ui Branch Testing

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tfdemo-front/",
+  base: "/tfdemo-front",
   plugins: [react(), tailwindcss()],
 })
