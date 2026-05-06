c404ac732c54904e66ec2d6d061bf6a2698f0a99
cubenx3-web <cubenx3@gmail.com>
2026-05-06 04:00:00 +0300

    fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/tfdemo-front",
+  base:"/",
   plugins: [react(), tailwindcss()],
 })
