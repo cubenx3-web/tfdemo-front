55fb9aee56116de487766c6b73c5e40218bd6e34
cubenx3-web <cubenx3@gmail.com>
2026-05-06 03:21:41 +0300

    ui Branch Testing

--- src/service/LogoutHandler.tsx
+++ src/service/LogoutHandler.tsx
@@ -1,22 +1,4 @@
-import { jwtDecode } from "jwt-decode";
 
 export function logout(){
-    localStorage.removeItem("token");
     window.location.href = "/login";
-}
-
-export function expTime(){
-    const token = localStorage.getItem("token");
-
-    if(token){
-        const decode = jwtDecode(token);
-        console.log(decode)
-        
-        const expTime = (Number(decode.exp)*1000) - Date.now()
-        return expTime;
-    }
-    else{
-        return 0
-    }
-
 }
