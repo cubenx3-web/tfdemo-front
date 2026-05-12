89c1102c80b02a83992005832c8931efc6ba2cc2
cubenx3-web <cubenx3@gmail.com>
2026-05-12 07:06:16 +0300

    admin dash, login an signUp page.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -51,7 +51,7 @@ function LoginPage(){
     return (
         <>  
             {/* login page container */}
-            <div className="flex-col relative space-y-5  bg-white  py-10  justify-center place-items-center rounded-xl">
+            <div className="flex-col relative space-y-5  bg-white px-3 py-10  justify-center place-items-center rounded-xl">
 
                 <div className="bg-white border-b-4 border-t-2 border-t-white p-3 rounded-full absolute -top-10 text-blue-700">
                     <BiUser size={70}/>
@@ -60,34 +60,34 @@ function LoginPage(){
                 <h2 className=" font-semibold text-2xl text-blue-600 text-center mt-10">Login</h2>
 
                 {/* Input */}
-                <form onSubmit={submitHandler} className="flex-col justify-center p-3 place-items-center space-y-7">
+                <form onSubmit={submitHandler} className="flex flex-col justify-center p-1 max-sm:text-sm place-items-center space-y-7">
 
-                    <div className="flex-col w-full justify-center place-items-center space-y-3">
-                        <div className="flex w-full justify-center place-items-center space-x-2 border border-gray-300 rounded-lg px-2">
+                    <div className="flex flex-col justify-center place-items-center space-y-3">
+                        <div className="flex justify-center place-items-center space-x-2 border border-gray-300 rounded-lg px-1">
                             <label className="font-medium text-lg flex place-items-center p-1 border-r border-gray-300" htmlFor="email">
                                 <MdOutlineEmail size={20}/>
                             </label>
-                            <input id="email" onChange={(inputHandler)} name="email"  value={values.email} className=" h-10 p-2 rounded flex-1 outline-blue-300" type="text" placeholder="Enter your email" required />
+                            <input id="email" onChange={(inputHandler)} name="email"  value={values.email} className=" h-10 p-1 rounded flex-1 outline-blue-300" type="text" placeholder="Enter your email" required />
                         </div>
 
-                        <div className="flex w-full justify-center place-items-center space-x-2 border border-gray-300 rounded-lg px-2">
+                        <div className="flex justify-center place-items-center space-x-2 border border-gray-300 rounded-lg px-1">
                             <label className="font-medium text-lg flex place-items-center p-1 border-r border-gray-300" htmlFor="password">
                                 <MdLockOutline size={20}/>
                             </label>
-                            <input id="password" onChange={(inputHandler)} name="password" className= " h-10 p-2 rounded flex-1 outline-blue-300" type="password" placeholder="Enter your password"/>
+                            <input id="password" onChange={(inputHandler)} name="password" className= " h-10 p-1 rounded flex-1 outline-blue-300" type="password" placeholder="Enter your password"/>
                         </div>
                     </div>
                     
                     
-                    <div className="flex-col w-full space-y-1.5 justify-center">
+                    <div className="flex flex-col w-full space-y-1 justify-center">
                         <button
                         type="submit"
-                        className="bg-blue-500 hover:bg-blue-700 active:bg-gray-500 text-white p-1.5 rounded-lg w-full font-semibold text-lg" >Login</button>
-                        <br />
+                        className="bg-blue-500 hover:bg-blue-700 active:bg-gray-500 text-white p-1 rounded-lg  font-semibold text-lg" >Login</button>
+                        
                         <h1 className="flex justify-center ">New here?</h1>
 
                         <button className="bg-blue-500
-                        hover:bg-blue-700 active:bg-gray-500 text-white p-1.5 rounded-lg w-full font-semibold text-lg" 
+                        hover:bg-blue-700 active:bg-gray-500 text-white p-1 rounded-lg  font-semibold text-lg" 
                         onClick={()=>(navigate("/sign-up"))}
                         >Create an Account</button>
                         
